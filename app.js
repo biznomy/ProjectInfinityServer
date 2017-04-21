f3a8bfa5c329d2e6b3806bf4d950460bb8cf2d9e
sdmmemberx1 <ajay.tlabs@gmail.com>
2017-04-21 10:41:07 +0530

    changes for testing

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(bodyParser.urlencoded({limit: '50mb',extended: true,parameterLimit:50000
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'store')));
 
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
